0fb8dd4a01191c87a5a99df9d61048c890b5a102
Sahat04 <jinssu@hotmail.com>
2026-03-16 18:38:43 +0100

    Implemented least privilege managed identity closes #9

--- provider.tf
+++ provider.tf
@@ -24,3 +24,17 @@ resource "azurerm_log_analytics_workspace" "monitor_law" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+# 1. Creates a "Robot Account" (Managed Identity) for future Python scripts
+resource "azurerm_user_assigned_identity" "monitor_identity" {
+  location            = azurerm_resource_group.monitor_rg.location
+  name                = "mi-earthquake-monitor"
+  resource_group_name = azurerm_resource_group.monitor_rg.name
+}
+
+# 2. Give the Robot Account ONLY permission to work with the Log Analytics database
+resource "azurerm_role_assignment" "monitor_role" {
+  scope                = azurerm_log_analytics_workspace.monitor_law.id
+  role_definition_name = "Log Analytics Contributor" 
+  principal_id         = azurerm_user_assigned_identity.monitor_identity.principal_id
+}
