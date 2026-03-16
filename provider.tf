af9de1fd2bd6c012800ea3e33dfa9dc5a7a9a6a6
Sahat04 <jinssu@hotmail.com>
2026-03-16 18:30:15 +0100

    Added Log Analytics Workspace for earthquake data closes #8

--- provider.tf
+++ provider.tf
@@ -15,3 +15,12 @@ resource "azurerm_resource_group" "monitor_rg" {
   name     = "rg-earthquake-monitor"
   location = "East US"
 }
+
+# Creates the database to store our earthquake logs
+resource "azurerm_log_analytics_workspace" "monitor_law" {
+  name                = "law-earthquake-monitor"
+  location            = azurerm_resource_group.monitor_rg.location
+  resource_group_name = azurerm_resource_group.monitor_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
